17445894e846c17703b8708fd605a931be38c442
Sebsterio <sebastian.rosloniec@gmail.com>
2019-12-21 21:17:06 +0100

    import export-events.js to every project

--- export-events.js
+++ export-events.js
@@ -1,24 +1,25 @@
-const SWIPE_SENSITIVITY = 15;
+(function() {
+	// distance between touchStart and touchEnd to be considered a swipe
+	const SWIPE_SENSITIVITY = 15;
 
-let touchstartY = 0;
-let touchendY = 0;
+	let touchstartY = 0;
 
-function handleTouchStart(e) {
-	touchstartY = e.touches[0].pageY;
-}
-function handleTouchEnd(e) {
-	touchendY = e.changedTouches[0].pageY;
-	const deltaY = touchendY - touchstartY;
-	const message =
-		deltaY > SWIPE_SENSITIVITY
-			? "swipe-down"
-			: deltaY < SWIPE_SENSITIVITY * -1
-			? "swipe-down"
-			: "touch";
-	targetOrigin = "*";
-	window.parent.postMessage(message, targetOrigin);
-}
+	function handleTouchStart(e) {
+		touchstartY = e.touches[0].pageY;
+	}
+	function handleTouchEnd(e) {
+		const touchendY = e.changedTouches[0].pageY;
+		const deltaY = touchendY - touchstartY;
+		const message =
+			deltaY > SWIPE_SENSITIVITY
+				? "swipe-down"
+				: deltaY < SWIPE_SENSITIVITY * -1
+				? "swipe-down"
+				: "touch";
+		targetOrigin = "*";
+		window.parent.postMessage(message, targetOrigin);
+	}
 
-document.addEventListener("touchstart", handleTouchStart);
-document.addEventListener("touchend", handleTouchEnd);
-// document.addEventListener("touchmove", handleTouchMove);
+	document.addEventListener("touchstart", handleTouchStart);
+	document.addEventListener("touchend", handleTouchEnd);
+})();
