6f24d261b7a5df9b9a835ef8ba88ea7e6d9dd3cc
Sebsterio <sebastian.rosloniec@gmail.com>
2019-12-27 16:51:04 +0100

    debounce postMessage instead of handleWheel

--- export-events.js
+++ export-events.js
@@ -1,11 +1,9 @@
+// problem: trackpad scroll event inertia keeps sending scroll event messages
+// attempted solution: window.parent: on scroll out, kill iframe script --> no success:
+// add attribute 'sandbox' --> no effect;
+// iframe src='' --> no effect
+
 (function() {
-	// distance between touchStart and touchEnd to be considered a swipe
-	const SWIPE_SENSITIVITY = 15;
-
-	// for debouncing scroll events - set high due to trackpad scroll inertia (the only solution I found)
-	const SCROLL_MIN_INTERVAL = 200;
-	const SCROLL_EDGE_INTERVAL = 2000;
-
 	function getDocHeight() {
 		const D = document;
 		return Math.max(
@@ -21,79 +19,71 @@
 	function isDocAtScrollStart() {
 		return window.scrollY <= 0;
 	}
+
 	function isDocAtScrollEnd() {
 		return window.scrollY + window.innerHeight >= getDocHeight();
 	}
 
+	// SWIPE
+
+	// distance between touchStart and touchEnd to be considered a swipe
+	const SWIPE_SENSITIVITY = 15;
+
 	let touchstartY = 0;
 	function handleTouchStart(e) {
 		touchstartY = e.touches[0].pageY;
-		console.log("touchstart");
 	}
+
 	function handleTouchEnd(e) {
 		const touchendY = e.changedTouches[0].pageY;
 		const deltaY = touchendY - touchstartY;
 		const message =
 			deltaY > SWIPE_SENSITIVITY
 				? isDocAtScrollStart()
-					? "swipe-down-edge"
-					: "swipe-down"
+					? "prev"
+					: ""
 				: deltaY < -SWIPE_SENSITIVITY
 				? isDocAtScrollEnd()
-					? "swipe-up-edge"
-					: "swipe-up"
+					? "next"
+					: ""
 				: "touch";
-		window.parent.postMessage(message, "*");
+		if (message) window.parent.postMessage(message, "*");
 	}
 
-	function handleWheel(e) {
-		const message =
-			e.deltaY > 0
-				? isDocAtScrollEnd()
-					? "scroll-down-edge"
-					: "scroll-down" // not reliable!
-				: e.deltaY < 0
-				? isDocAtScrollStart()
-					? "scroll-up-edge"
-					: "scroll-up" // not reliable!
-				: "";
-		if (!message) return;
+	// SCROLL
+
+	// for debouncing scroll event messages - set high due to trackpad scroll inertia (the only solution I found)
+	const SCROLL_MESSAGE_INTERVAL = 2000;
+
+	let timeout;
+	let ready = true;
+
+	function debouncedMessage(message) {
+		ready = false;
+		clearTimeout(timeout);
+		timeout = setTimeout(() => {
+			ready = true;
+		}, SCROLL_MESSAGE_INTERVAL);
+
 		window.parent.postMessage(message, "*");
 		console.log(message);
 	}
 
-	// problem: trackpad scroll event inertia keeps sending scroll event messages
-	// attempted solution: window.parent: on scroll out, kill iframe script --> no success:
-	// add attribute 'sandbox' --> no effect;
-	// iframe src='' --> no effect
-
-	let timeout;
-	let ready = true;
-	function debounce(e, callback) {
-		if (ready) {
-			ready = false;
-			clearTimeout(timeout);
-
-			// edge scroll -> long interval
-			if (isDocAtScrollEnd() || isDocAtScrollStart())
-				timeout = setTimeout(() => {
-					ready = true;
-				}, SCROLL_EDGE_INTERVAL);
-			// mid-doc scroll -> short interval
-			else
-				timeout = setTimeout(() => {
-					ready = true;
-				}, SCROLL_MIN_INTERVAL);
-			callback(e);
-		} else {
+	// send message only when doc is scrolled to the top/bottom edge and user scrolling towards the edge
+	function handleWheel(e) {
+		if (!ready) {
 			console.log("not ready");
+			return;
+		}
+		if (isDocAtScrollEnd() && e.deltaY > 0) {
+			debouncedMessage("next");
+		} else if (isDocAtScrollStart() && e.deltaY < 0) {
+			debouncedMessage("prev");
 		}
 	}
 
-	window.addEventListener("touchstart", handleTouchStart);
-	window.addEventListener("touchend", handleTouchEnd);
-	window.addEventListener("wheel", e => debounce(e, handleWheel));
+	// use `document` and not `window` in case some projects' <html> height is not 100vh
 	document.addEventListener("touchstart", handleTouchStart);
 	document.addEventListener("touchend", handleTouchEnd);
-	document.addEventListener("wheel", e => debounce(e, handleWheel));
+	document.addEventListener("wheel", handleWheel);
 })();
